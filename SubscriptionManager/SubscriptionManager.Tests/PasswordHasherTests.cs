0f0ad407750042771868a9815cc429d3baf989a6
am1xol <ruslannikitsin@gmail.com>
2025-11-09 15:08:40 +0300

    add comprehensive unit tests

---
+++ SubscriptionManager/SubscriptionManager.Tests/PasswordHasherTests.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Extensions.Options;
+using SubscriptionManager.Core.Options;
+using SubscriptionManager.Infrastructure.Services;
+using Xunit;
+
+namespace SubscriptionManager.Tests;
+
+public class PasswordHasherTests
+{
+    private readonly CustomPasswordHasher _passwordHasher;
+
+    public PasswordHasherTests()
+    {
+        var options = Options.Create(new PasswordHasherOptions());
+        _passwordHasher = new CustomPasswordHasher(options);
+    }
+
+    [Fact]
+    public void HashPassword_ShouldReturnDifferentHashes_ForSamePassword()
+    {
+        var password = "TestPassword123!";
+        var hash1 = _passwordHasher.HashPassword(password);
+        var hash2 = _passwordHasher.HashPassword(password);
+        Assert.NotEqual(hash1, hash2);
+    }
+
+    [Fact]
+    public void VerifyPassword_ShouldReturnTrue_ForCorrectPassword()
+    {
+        var password = "TestPassword123!";
+        var hash = _passwordHasher.HashPassword(password);
+        var result = _passwordHasher.VerifyPassword(password, hash);
+        Assert.True(result);
+    }
+}
